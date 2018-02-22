53c3a10df6c28f66c19b102b5721cd489428e53c
Jorge Ramirez <jramirez@cambridgeassociates.com>
2018-02-22 09:00:31 -0600

    Candidate voted flag subscribed with async instead literally

--- src/app/components/candidate/candidate.component.ts
+++ src/app/components/candidate/candidate.component.ts
@@ -1,25 +1,17 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import {Candidate} from "./candidate";
-import {AuthService} from "../../shared/services/auth.service";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Candidate} from './candidate';
+import {AuthService} from '../../shared/services/auth.service';
 
 @Component({
   selector: 'candidate',
   templateUrl: './candidate.component.html',
   styleUrls: ['./candidate.component.scss']
 })
-export class CandidateComponent implements OnInit {
+export class CandidateComponent {
   @Input('data') data: Candidate;
   @Output() candidateVoted = new EventEmitter<string>();
 
-  voteEnabled: boolean = false;
-
-  constructor(private auth: AuthService) {}
-
-  ngOnInit() {
-    this.auth.user.subscribe(({voted = false}) => {
-      this.voteEnabled = !voted;
-    });
-  }
+  constructor(public auth: AuthService) {}
 
   voteCandidate(id: string) {
     this.candidateVoted.emit(id);
